8ae5fdcb814297adb0d254008619e332db935141
Adam <adampugh@zoho.com>
2018-11-23 16:28:02 +0000

    Add new page

---
+++ src/pages/new.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewPage extends Component {
+    componentDidMount() {
+        alert('this has mounted');
+    }
+
+    render() {
+        return(
+            <div>This is it</div>
+        )
+    }
+}
+
+export default NewPage;
