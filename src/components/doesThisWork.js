8c398d106e8b45b5af075224f91211f81bda4074
Adam <adampugh@zoho.com>
2018-11-23 16:37:13 +0000

    trial

--- src/components/doesThisWork.js
+++ src/components/doesThisWork.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
-class DoesThisWork extends Component {
-    componentDidMount() {
-        alert('this will mount');
-    }
+// class DoesThisWork extends Component {
+//     componentDidMount() {
+//         alert('this will mount');
+//     }
 
-    render() {
-        return (
-            <div>hello this rendered</div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>hello this rendered</div>
+//         )
+//     }
+// }
+
+const DoesThisWork = () => (
+    <h1>ello</h1>
+)
 
 export default DoesThisWork;
